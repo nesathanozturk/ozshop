cf53b056cac2558e265057b4f0c24d127d98ae4e
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-05-18 17:41:19 +0300

    feat: created featurecard

--- src/modules/home/Home.jsx
+++ src/modules/home/Home.jsx
@@ -1,3 +1,4 @@
+import FeatureCard from "../../components/featureCard/FeatureCard";
 import Hero from "../../components/hero/Hero";
 import Products from "../../components/products/Products";
 
@@ -5,8 +6,16 @@ const Home = () => {
   return (
     <>
       <Hero />
-      <h2 className="text-4xl font-bold text-center mt-20">Products</h2>
+      <div className="flex flex-col text-center w-full mt-20">
+        <h2 className="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">
+          Products
+        </h2>
+        <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900">
+          Most Popular Products
+        </h1>
+      </div>
       <Products />
+      <FeatureCard />
     </>
   );
 };
