3bf825af9fdebff81579f80cc303ec7d879a584e
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-05-18 21:06:21 +0300

    feat: created route for cart

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { Routes, Route } from "react-router-dom";
+import Cart from "./pages/cart/Cart";
+import CategoryProducts from "./pages/category-products/CategoryProducts";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import Home from "./pages/home/Home";
 import ProductDetail from "./pages/product-detail/ProductDetail";
 import Products from "./pages/products/Products";
-import CategoryProducts from "./pages/category-products/CategoryProducts";
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         <Route path="/products" exact element={<Products />} />
         <Route path="/product/:id" element={<ProductDetail />} />
         <Route path="/categories/:name" element={<CategoryProducts />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
       <Footer />
     </>
