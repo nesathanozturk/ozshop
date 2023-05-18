f5df6e34311fa4419318657692b7f693ad8752d2
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-05-18 17:32:49 +0300

    feat: import home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/header/Header";
+import Home from "./modules/home/Home";
 
 const App = () => {
   return (
     <>
       <Header />
+      <Home />
     </>
   );
 };
--- src/modules/home/Home.jsx
+++ src/modules/home/Home.jsx
@@ -1,5 +1,11 @@
+import Hero from "../../components/header/hero/Hero";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <Hero />
+    </>
+  );
 };
 
 export default Home;
