dbfe234ed4d935c1a2acaf995fcd238b08e954a5
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-05-18 22:59:49 +0300

    style: edited hero

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,25 +1,18 @@
 const Hero = () => {
   return (
-    <section className="text-gray-600 body-font mt-20">
+    <section className="text-gray-600 body-font">
       <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-            Before they sold out
-            <br className="hidden lg:inline-block" />
-            readymade gluten
+            Huge Summer Collection
           </h1>
           <p className="mb-8 leading-relaxed">
-            Copper mug try-hard pitchfork pour-over freegan heirloom neutra air
-            plant cold-pressed tacos poke beard tote bag. Heirloom echo park
-            mlkshk tote bag selvage hot chicken authentic tumeric truffaut
-            hexagon try-hard chambray.
+            Everything you need for the summer! Swimmers, bikinis, shorts,
+            t-shirts, flip-flops, sunglasses, hats, and more!
           </p>
           <div className="flex justify-center">
             <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-              Button
-            </button>
-            <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
-              Button
+              Show Now
             </button>
           </div>
         </div>
@@ -27,7 +20,7 @@ const Hero = () => {
           <img
             className="object-cover object-center rounded"
             alt="hero"
-            src="https://dummyimage.com/720x600"
+            src="https://www.bethesdatailors.com/wp-content/uploads/2021/01/Reliable-Clothes-Store-for-All-in-NSW.jpg"
           />
         </div>
       </div>
