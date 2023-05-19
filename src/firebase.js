ca5b8cc813dc19a62d0df311aa08a1a0d34ccf6d
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-05-19 20:51:39 +0300

    feat: added firebase

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDb1eS_wEoipy5etvXH8l0NGa3Wi4Nrzfo",
+
+  authDomain: "nesatbaba-app.firebaseapp.com",
+
+  projectId: "nesatbaba-app",
+
+  storageBucket: "nesatbaba-app.appspot.com",
+
+  messagingSenderId: "254752846892",
+
+  appId: "1:254752846892:web:d0cab812d7edb2e5f6cd89",
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
