69ccc6e021928cfe58bfab3bd5d4c101075262aa
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-05-19 13:57:10 +0300

    feat: added axios

--- src/pages/category-products/CategoryProducts.jsx
+++ src/pages/category-products/CategoryProducts.jsx
@@ -1,6 +1,9 @@
+import axios from "axios";
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import ProductCard from "../../components/product-card/ProductCard";
+import Title from "../../components/title/Title";
+import Loading from "../../components/loading/Loading";
 
 const CategoryProducts = () => {
   const [products, setProducts] = useState([]);
@@ -8,19 +11,37 @@ const CategoryProducts = () => {
   const { name } = useParams();
 
   useEffect(() => {
-    const fetchCategory = async () => {
-      const response = await fetch(
-        `https://fakestoreapi.com/products/category/${name}`
-      );
-      const data = await response.json();
-      setProducts(data);
+    const getCategory = async () => {
+      try {
+        const res = await axios.get(
+          `https://fakestoreapi.com/products/category/${name}`,
+          {
+            method: "GET",
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        const data = await res.data;
+        setProducts(data);
+      } catch (err) {
+        throw new Error(err);
+      }
     };
-    fetchCategory();
+    getCategory();
   }, []);
 
-  if (products.length === 0) return <div>Loading...</div>;
-
-  return <ProductCard products={products} />;
+  return (
+    <>
+      <Title title="Category" titleAlt={name} />
+      {products.length === 0 ? (
+        <Loading />
+      ) : (
+        <ProductCard products={products} />
+      )}
+    </>
+  );
 };
 
 export default CategoryProducts;
