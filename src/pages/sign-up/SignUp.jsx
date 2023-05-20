0b61d43cb413f0ff0471602fb2362bcb82adbe28
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-05-20 17:13:26 +0300

    feat: added fucn

--- src/pages/sign-up/SignUp.jsx
+++ src/pages/sign-up/SignUp.jsx
@@ -1,5 +1,102 @@
+import { Link } from "react-router-dom";
+import { auth } from "../../firebase";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { useState } from "react";
+import Loading from "../../components/loading/Loading";
+
 const SignUp = () => {
-  return <div>SignUp</div>;
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [createUserWithEmailAndPassword, loading, error] =
+    useCreateUserWithEmailAndPassword(auth);
+
+  if (error) return console.log("Error:", error);
+
+  if (loading) return <Loading />;
+
+  const handleSignUp = () => {
+    if (username === "" || email === "" || password === "") {
+      alert("Please fill in all fields");
+    } else {
+      createUserWithEmailAndPassword(email, password);
+    }
+  };
+
+  return (
+    <>
+      <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
+        <form className="space-y-6" action="#">
+          <h5 className="text-xl font-medium text-gray-900 dark:text-white">
+            Sign in to our platform
+          </h5>
+          <div>
+            <label
+              htmlFor="username"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Your username
+            </label>
+            <input
+              type="username"
+              name="username"
+              id="username"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+              placeholder="Your username"
+              required
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="email"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Your email
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+              placeholder="Your email"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="password"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Your password
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Your password"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <Link to="/login">You have already an account? Log in!</Link>
+          <button
+            type="submit"
+            className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            onClick={handleSignUp}
+          >
+            Sign up
+          </button>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default SignUp;
