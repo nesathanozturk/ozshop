2fe40a369cffcaa9a5c6cd9d7864a1ccc71082e9
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-05-20 17:22:19 +0300

    feat: added func

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,5 +1,18 @@
+import { Outlet, Navigate } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../../firebase";
+import Loading from "../loading/Loading";
+
 const Main = () => {
-  return <div>Main</div>;
+  const [user, loading, error] = useAuthState(auth);
+
+  if (error) return console.log("Error:", error);
+
+  if (loading) return <Loading />;
+
+  if (!user) return <Navigate to="/login" replace />;
+
+  return <Outlet />;
 };
 
 export default Main;
