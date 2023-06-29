cca7581428dcc6d679ad1e53cffd535b5addcc47
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-06-29 12:30:52 +0300

    style: edited testimonnial

--- src/components/testimonial/index.js
+++ src/components/testimonial/index.js
@@ -1,27 +1,67 @@
 const Testimonial = () => {
   return (
-    <section class="text-gray-600 body-font">
-      <div class="container px-5 py-24 mx-auto">
-        <div class="xl:w-1/2 lg:w-3/4 w-full mx-auto text-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            class="inline-block w-8 h-8 text-gray-400 mb-8"
-            viewBox="0 0 975.036 975.036"
-          >
-            <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
-          </svg>
-          <p class="leading-relaxed text-lg">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet,
-            labore animi. Alias est rem numquam itaque ratione porro voluptatum
-            accusantium. Iure consequuntur odit qui illum exercitationem atque
-            enim asperiores excepturi!
-          </p>
-          <span class="inline-block h-1 w-10 rounded bg-indigo-500 mt-8 mb-6"></span>
-          <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">
-            BUĞRAHAN ÖZTÜRK
-          </h2>
-          <p class="text-gray-500">Backend Developer</p>
+    <section className="text-gray-600 body-font">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="flex flex-wrap -m-4">
+          <div className="lg:w-1/3 lg:mb-0 mb-6 p-4">
+            <div className="h-full text-center">
+              <img
+                alt="Jeff Bezos"
+                className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100"
+                src="https://upload.wikimedia.org/wikipedia/commons/0/03/Jeff_Bezos_visits_LAAFB_SMC_%283908618%29_%28cropped%29.jpeg"
+              />
+              <p className="leading-relaxed">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Corrupti, necessitatibus ullam voluptatum a provident molestiae
+              </p>
+              <span className="inline-block h-1 w-10 rounded bg-indigo-500 mt-6 mb-4"></span>
+              <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm">
+                JEFF BEZOS
+              </h2>
+              <p className="text-gray-500">Product Manager</p>
+            </div>
+          </div>
+          <div className="lg:w-1/3 lg:mb-0 mb-6 p-4">
+            <div className="h-full text-center">
+              <img
+                alt="Elon Musk"
+                className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100"
+                src="https://pyxis.nymag.com/v1/imgs/f97/52e/6a9067e484d6bf38587100fd223c0c229d-12-elon-musk.2x.h473.w710.jpg"
+              />
+              <p className="leading-relaxed">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Aliquam omnis doloremque eos perspiciatis quam, numquam
+                consectetur. Eveniet quam libero inventore numquam doloremque
+                nobis nesciunt laudantium saepe ex nulla, veritatis cupiditate
+                eligendi dolores asperiores ullam eum neque optio voluptates?
+                Molestias, nesciunt!
+              </p>
+              <span className="inline-block h-1 w-10 rounded bg-indigo-500 mt-6 mb-4"></span>
+              <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm">
+                ELON MUSK
+              </h2>
+              <p className="text-gray-500">CEO</p>
+            </div>
+          </div>
+          <div className="lg:w-1/3 lg:mb-0 p-4">
+            <div className="h-full text-center">
+              <img
+                alt="Mark Zuckerberg"
+                className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100"
+                src="https://i0.wp.com/www.entrepreneurs.ng/wp-content/uploads/2019/06/Mark-Zuckerberg.jpg?fit=800%2C450&ssl=1"
+              />
+              <p className="leading-relaxed">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Delectus assumenda iste nulla pariatur fuga voluptas aliquid
+                suscipit vero vitae, non inventore eius consequatur.
+              </p>
+              <span className="inline-block h-1 w-10 rounded bg-indigo-500 mt-6 mb-4"></span>
+              <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm">
+                MARK ZUCKERBERG
+              </h2>
+              <p className="text-gray-500">Senior Frontend Developer</p>
+            </div>
+          </div>
         </div>
       </div>
     </section>
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -5,6 +5,7 @@ import StatsCard from "../../components/stats-card";
 import Loading from "../../components/loading";
 import Title from "../../components/title";
 import ProductCard from "../../components/product-card";
+import Testimonial from "../../components/testimonial";
 
 const Home = () => {
   const { apiData } = useFetch("https://fakestoreapi.com/products?limit=7");
@@ -18,6 +19,7 @@ const Home = () => {
       {apiData?.length > 0 ? <ProductCard products={apiData} /> : <Loading />}
       <ProductCard />
       <StatsCard />
+      <Testimonial />
     </>
   );
 };
