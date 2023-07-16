71040072356fce5fc19012667e441940980b4279
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-07-16 18:07:10 +0300

    chore: removed inputs

--- src/components/ui/CartProduct.jsx
+++ src/components/ui/CartProduct.jsx
@@ -25,11 +25,9 @@ const CartProduct = ({ cart, handleChangeAmount }) => {
             >
               <path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
             </svg>
-            <input
-              className="mx-2 border text-center w-8"
-              type="text"
-              value={cart?.quantity}
-            />
+            <span className="mx-2 border text-center w-8">
+              {cart?.quantity}
+            </span>
             <svg
               className="fill-current text-gray-600 w-3 cursor-pointer"
               onClick={() => handleChangeAmount(cart, 1)}
