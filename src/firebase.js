1a5d1b235018e0b6e325d51af4c4be9785d27c64
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-08-20 14:44:03 +0300

    feat: add env

--- src/firebase.js
+++ src/firebase.js
@@ -2,17 +2,12 @@ import { initializeApp } from "firebase/app";
 import { GoogleAuthProvider, getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDb1eS_wEoipy5etvXH8l0NGa3Wi4Nrzfo",
-
-  authDomain: "nesatbaba-app.firebaseapp.com",
-
-  projectId: "nesatbaba-app",
-
-  storageBucket: "nesatbaba-app.appspot.com",
-
-  messagingSenderId: "254752846892",
-
-  appId: "1:254752846892:web:d0cab812d7edb2e5f6cd89",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
